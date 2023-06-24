5b11f8fdabb5eb97957d6424fac03a164ad680b1
Alan Salazar <alan.salazar@alansal-ltmyyju.internal.salesforce.com>
2023-06-23 20:12:58 -0700

    project

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,25 @@
-// YOUR CODE HERE
+//YOUR CODE GOES HERE
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const db = require('./data/db.json')
+
+app.use(cors())
+app.use(express.json())
+
+
+//Get request
+app.get('/', (req, res) => {
+    res.status(200).send(db.products)
+})
+
+//post request
+app.post('/products', (req, res) => {
+    res.status(200).send(req.body)
+})
+
+
+
+
+
+module.exports = app;
